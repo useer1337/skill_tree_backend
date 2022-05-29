aca9fb06e550971c8b11347e1ac96be64968b9ef
Koval Alexander <koval@hostco.ru>
2022-05-29 21:19:33 +0500

    init project

---
+++ src/main/java/com/example/skill_tree_backend/SkillTreeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.skill_tree_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillTreeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillTreeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skill_tree_backend/SkillTreeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skill_tree_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillTreeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
